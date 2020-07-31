d0df6b8352ccc10778a7ac79fdc58bd4e1906b0f
HAB27 <you@example.com>
2020-07-31 11:47:53 +0200

    ejercico da algo de asko pero hablando funcionalmetne, funchionen!!!! haha

---
+++ primerrepositorio/entregasHAB/mod7.frontend/Proyectos/hackamarketfase2.0/hackamusik/hackamusic/src/components/TagCard.vue
@@ -0,0 +1,26 @@
+<template>
+   <div class="TagCard">
+    <h1>Artistas</h1>
+    <ul>
+      <li v-for="tag in toptags" :key="tag.id">
+        <p>{{ tag.name }}</p>
+        <a href="tag.url">{{ tag.url }}</a>
+        <p>Nº Etiquetas: {{ tag.taggings }}</p>
+      </li>
+    </ul>
+
+  </div>
+</template>
+<script>
+export default {
+    name: "TagCard",
+    props: {
+        toptags: Array
+    }
+}
+</script>
+
+<style scoope>
+
+
+</style>
---
+++ primerrepositorio/entregasHAB/mod7.frontend/Proyectos/hackamarketfase2.0/hackamusik/hackamusic/src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <vue-headful
+      title="Hack a Music | Home"
+      />
+    <h1>HACK A MUSIIC</h1>
+    <toptags :toptags="toptags"/>
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import api from '@/api/index.js';
+//importo component TagCard
+import toptags from '@/components/TagCard.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    toptags
+  },
+  data(){
+    return{
+      toptags:[]
+    }
+  },
+
+//HOOK
+created(){
+  console.log('Created');
+  //del data recibimos la informacion de los artistas 
+  api.getTopTags().then(response => (this.toptags = response.data.tags.tag))
+  
+}
+}
+</script>
